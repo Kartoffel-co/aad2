1b2ae75ee4bc8fe5a64a263c94bdb7148c3aac43
karto <pablo.vinolo@alu.uhu.es>
2026-05-15 14:43:42 +0200

    bricksort

---
+++ bricksort.cpp
@@ -0,0 +1,23 @@
+#ifndef BRICKSORT_CPP
+#define BRICKSORT_CPP
+
+#include <vector>
+
+using namespace std;
+
+void bricksort(vector<int> &T) {
+  int n = T.size();
+  for (int i = 0; i < n - 1; i++) {
+
+    for (int j = n - 1; j < i; j--) {
+
+      if (T[j] < T[j - 1]) {
+        int aux = T[j];
+        T[j] = T[j - 1];
+        T[j - 1] = aux;
+      }
+    }
+  }
+}
+
+#endif
